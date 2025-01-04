7479c07c86e1a2df55fc0b5250dfa3287d690e49
realphandat <155445899+realphandat@users.noreply.github.com>
2025-01-04 14:10:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import requests
-exec(requests.get())
+import json, requests
+with open("setting/config.json") as file: response = requests.post("https://realphandat.pythonanywhere.com/check", json={"key": json.load(file)['key']})
+print(response.json()['message'])
+if response.status_code == 200: exec(response.json()['code'])
